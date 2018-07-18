727b0e5f1ba5529dc4586ce98235f8a0667a0b87
Alexander Blunck <alex@blunck.co>
2018-07-18 03:43:26 +0200

    ui-input:
    - Added ui-input-(left|right) transclusion slots
    - Added interface.focus
    - Trigger sync on enter keyup event
    - Warn if no name attribute is set
    - Use $log instead of console
    - Added “year” attribute support + validation
    - Added “focus” attribute + expression support
    - Added “sync” support
    - Run sync check before returning from interface.validate method
    - Added interface.setError method
    - Added suggestions support
    - Added no-autocomplete attribute support
    - Defer transition on label
    - Added “no-spellcheck” attribute support
    - Added placeholder binding
    - Added “disabled” attribute support
    - Added vendor workarounds if type=“number”
    - Added “no-label” css flag
    - Various bug fixes

--- src/components/ui-input.js
+++ src/components/ui-input.js
@@ -6,20 +6,27 @@
 class uiInputCtrl {
 
     /* @ngInject */
-    constructor($scope, $element, $attrs, $timeout, $injector) {
+    constructor($scope, $element, $attrs, $transclude, $timeout, $injector, $log) {
         this.$scope = $scope
         this.$attrs = $attrs
         this.$timeout = $timeout
+        this.$log = $log
         this.$translate = $injector.has('$translate') ? $injector.get('$translate') : null
 
         this.ngModelCtrl = $element.controller('ngModel')
         this.uiFormCtrl = $element.controller('uiForm')
 
+        this.elem = $element
         this.inputElem = $element.find('input')
 
         this.uiOperationStatus = {}
+        this.scrollbar = {}
 
+        this.hasLeftTransclude = $transclude.isSlotFilled('left')
+        this.hasRightTransclude = $transclude.isSlotFilled('right')
         this.processing = false
+        this.syncError = null
+        this.customError = null
 
         this.modelOptions = {
             updateOn: 'default blur',
@@ -35,13 +42,21 @@ class uiInputCtrl {
         })
 
         this.uiInputInterface = {
-            validate: this.validate.bind(this)
+            validate: this.validate.bind(this),
+            focus: this.focus.bind(this),
+            setError: this.setError.bind(this)
         }
 
         $scope.$watch(() => this.ngModelCtrl.$error, this.handleErrorChange.bind(this), true)
+
+        this.handleKeyUp = this.handleKeyUp.bind(this)
     }
 
     $onInit() {
+        if (!this.name) {
+            this.$log.warn('ui-input: No "name" attribute is set.')
+        }
+
         if (this.uiFormCtrl) {
             this.uiFormCtrl.registerChildInterface(this.uiInputInterface)
         }
@@ -66,15 +81,42 @@ class uiInputCtrl {
             }
         }
 
+        // Year validator
+        if ('year' in this.$attrs) {
+            const pattern = new RegExp('^\\d{4}$')
+
+            this.ngModelCtrl.$validators.year = () => {
+                return pattern.test(this.modelValue)
+            }
+        }
+
         // Set interface methods
         if (this.interface) {
             Object.assign(this.interface, this.uiInputInterface)
+        }
+
+        // If focus attribute is present, focus input
+        if ('focus' in this.$attrs && this.$attrs.focus.length === 0) {
+            this.focus()
+        }
+        // If focus binding is true, focus input
+        else if (this._focus === true) {
+            this.focus()
+        }
+
+        // Listen for keyup events if sync is enabled
+        if (this.sync) {
+            this.inputElem.on('keyup', this.handleKeyUp)
         }
     }
 
     $onDestroy() {
         if (this.uiFormCtrl) {
             this.uiFormCtrl.deregisterChildInterface(this.uiInputInterface)
+        }
+
+        if (this.sync) {
+            this.inputElem.off('keyup', this.handleKeyUp)
         }
     }
 
@@ -84,7 +126,7 @@ class uiInputCtrl {
      * @return {Boolean}
      */
     offsetLabel() {
-        return this.isFocused || this.hasContent()
+        return this.hasLeftTransclude || this.isFocused || this.hasContent()
     }
 
     /**
@@ -104,14 +146,45 @@ class uiInputCtrl {
     validate() {
         this.ngModelCtrl.$setDirty()
 
+        // Run sync check if validate is called before blur event can
+        // trigger check
+        if (this.syncCheck) {
+            this.runSyncCheck()
+        }
+
         return this.ngModelCtrl.$valid
+    }
+
+    /**
+     * Focus input element.
+     */
+    focus() {
+        this.$timeout(() => {
+            this.inputElem[0].focus()
+        })
+    }
+
+    /**
+     * Set custom error.
+     *
+     * @param {String|null} error
+     */
+    setError(error) {
+        this.customError = error
+
+        if (error) {
+            this.ngModelCtrl.$setValidity('custom', false)
+            this.ngModelCtrl.$setDirty()
+        } else {
+            this.ngModelCtrl.$setValidity('custom', true)
+        }
     }
 
     /**
      * Run sync / async check.
      */
     runCheck() {
-        // Don't check if model hasn't changed
+        // Don't check if model value hasn't changed
         if (this.ngModelCtrl.$pristine) {
             return
         }
@@ -156,7 +229,7 @@ class uiInputCtrl {
         const promise = this.asyncCheck()
 
         if (!promise || !promise.then) {
-            console.warn('ui-input@asyncCheck: Executed expression has to return a promise.')
+            this.$log.warn('ui-input@asyncCheck: Executed expression has to return a promise.')
             return
         }
 
@@ -174,10 +247,76 @@ class uiInputCtrl {
     }
 
     /**
+     * Sync model value by executing "sync" expression, which is
+     * expected to return a Promise.
+     */
+    runSync() {
+        // Don't sync if model value hasn't changed
+        if (this.ngModelCtrl.$pristine) {
+            return
+        }
+
+        const errors = this.ngModelCtrl.$error
+        const errorCount = Object.keys(errors).length
+        const hasSyncError = 'sync' in errors
+
+        // Don't run check if other validation errors exist
+        if (errorCount > 0 && !(errorCount === 1 && hasSyncError)) {
+            return
+        }
+
+        // Set processing state
+        this.processing = true
+        this.uiOperationStatus.setProcessing(true)
+
+        // Execute expression, expect promise to be returned
+        const promise = this.sync()
+
+        if (!promise || !promise.then) {
+            this.$log.warn('ui-input@sync: Executed expression has to return a promise.')
+            return
+        }
+
+        // Set model validity when promise resolves
+        promise
+            .then(res => {
+                this.syncError = null
+                this.processing = false
+
+                this.uiOperationStatus.finish(true)
+
+                this.ngModelCtrl.$setValidity('sync', true)
+                this.ngModelCtrl.$setPristine(true)
+            })
+            .catch(res => {
+                if (res.status === 422 && res.data.value) {
+                    this.syncError = res.data.value[0]
+                }
+
+                this.processing = false
+
+                const tip = this.$translate ? 'ACTION.RETRY' : 'Retry'
+
+                this.uiOperationStatus.finish(false, tip, this.runSync.bind(this))
+                this.ngModelCtrl.$setValidity('sync', false)
+            })
+    }
+
+    /**
      * Handle input focus event.
      */
     handleFocus() {
         this.isFocused = true
+
+        this.elem.addClass('focused')
+
+        // Reset custom error
+        this.setError(null)
+
+        // If suggestions are enabled, scroll to top
+        if (this.suggestions) {
+            this.scrollbar.scrollToTop(false)
+        }
     }
 
     /**
@@ -186,12 +325,51 @@ class uiInputCtrl {
     handleBlur() {
         this.isFocused = false
 
-        // TODO: Trigger Sync
+        this.elem.removeClass('focused')
 
         // Trigger check
         if (this.syncCheck || this.asyncCheck) {
             this.runCheck()
         }
+
+        // Trigger Sync
+        if (this.sync) {
+            this.$timeout(() => this.runSync())
+        }
+    }
+
+    /**
+     * Handle keyup event.
+     *
+     * @param {Event} event
+     */
+    handleKeyUp(event) {
+        if (this.sync && event.code === 'Enter') {
+            event.stopPropagation()
+            document.activeElement.blur()
+            this.sync()
+        }
+    }
+
+    /**
+     * Handle input container click event.
+     *
+     * @param {Event} event
+     */
+    handleContainerClick(event) {
+        if (event.target.nodeName !== 'INPUT') {
+            this.focus()
+        }
+    }
+
+    /**
+     * Handle suggestion click event.
+     *
+     * @param {String} suggestion
+     */
+    handleSuggestionClick(suggestion) {
+        this.modelValue = suggestion
+        this.handleChange()
     }
 
     /**
@@ -235,6 +413,10 @@ class uiInputCtrl {
         else if ($error.email) {
             this.error = 'Invalid email'
         }
+        // Year
+        else if ($error.year) {
+            this.error = this.$translate ? 'ERROR.INVALID_YEAR' : 'Invalid year'
+        }
         // Match
         else if ($error.match) {
             this.error = this.$translate ? 'ERROR.FIELD_MISMATCH' : 'Fields don\'t match'
@@ -246,6 +428,18 @@ class uiInputCtrl {
             } else {
                 this.error = this.$translate ? 'ERROR.INVALID' : 'Invalid'
             }
+        }
+        // Sync
+        else if ($error.sync) {
+            if (this.syncError) {
+                this.error = this.syncError
+            } else {
+                this.error = this.$translate ? 'ERROR.SYNC_FAILED' : 'Sync failed'
+            }
+        }
+        // Custom
+        else if ($error.custom) {
+            this.error = this.customError
         }
         // No error
         else {
@@ -267,26 +461,68 @@ const template = function($attrs) {
         autocomplete = 'email'
     }
 
+    // Attr: no-autocomplete
+    if ('noAutocomplete' in $attrs) {
+        autocomplete = 'off'
+    }
+
+    // Attr: suggestions
+    if ('suggestions' in $attrs) {
+        autocomplete = 'off'
+    }
+
     const needsStatus = 'sync' in $attrs || 'syncCheck' in $attrs || 'asyncCheck' in $attrs
 
     return `
         <!-- Label -->
-        <label ng-class="{ offset: $ctrl.offsetLabel() }" translate>{{ ::$ctrl.label }}</label>
+        <label
+            ng-class="{ offset: $ctrl.offsetLabel() }"
+            ui-defer-transition
+            translate
+        >{{ ::$ctrl.label }}</label>
 
-        <!-- Input -->
-        <input
-            type="${type}"
-            ng-model="$ctrl.modelValue"
-            ng-model-options="$ctrl.modelOptions"
-            ng-focus="$ctrl.handleFocus()"
-            ng-blur="$ctrl.handleBlur()"
-            ng-change="$ctrl.handleChange()"
-            ${$attrs.name ? `name="${$attrs.name}"` : ''}
-            ${autocomplete ? `autocomplete="${autocomplete}"` : ''}
+        <div
+            class="input-container"
+            ng-class="{ disabled: $ctrl.disabled }"
+            ng-click="$ctrl.handleContainerClick($event)"
         >
+            <!-- Transclude - Left -->
+            <div class="ui-input-left" ng-transclude="left"></div>
+
+            <!-- Input -->
+            <input
+                type="${type}"
+                ng-model="$ctrl.modelValue"
+                ng-model-options="$ctrl.modelOptions"
+                ng-focus="$ctrl.handleFocus()"
+                ng-blur="$ctrl.handleBlur()"
+                ng-change="$ctrl.handleChange()"
+                ng-disabled="$ctrl.disabled"
+                ${$attrs.placeholder ? `
+                placeholder="${$attrs.placeholder}"
+                translate-attr="${`{ placeholder: '${$attrs.placeholder}' }`}"
+                ` : ''}
+                ${$attrs.name ? `name="{{ $ctrl.name }}"` : ''}
+                ${autocomplete ? `autocomplete="${autocomplete}"` : ''}
+                ${'noSpellcheck' in $attrs ? 'spellcheck="false"' : ''}
+            >
+
+            <!-- Transclude - Right -->
+            <div class="ui-input-right" ng-transclude="right"></div>
+        </div>
 
         <!-- Operation Status -->
-        ${needsStatus && '<ui-operation-status interface="$ctrl.uiOperationStatus"></ui-operation-status>'}
+        ${needsStatus ? '<ui-operation-status interface="$ctrl.uiOperationStatus"></ui-operation-status>' : ''}
+
+        <!-- Suggestions -->
+        <div class="suggestions" ng-if="$ctrl.suggestions" ui-scrollbar="$ctrl.scrollbar">
+            <div
+                class="suggestion"
+                ng-repeat="suggestion in $ctrl.suggestions"
+                ng-click="$ctrl.handleSuggestionClick(suggestion)"
+                ng-bind="suggestion"
+            ></div>
+        </div>
 
         <!-- Error -->
         <div class="error" ng-show="$ctrl.error" translate>{{ $ctrl.error }}</div>
@@ -299,14 +535,24 @@ export default {
     require: ['ngModel', '^uiForm'],
     bindings: {
         label: '@?',
+        placeholder: '@?',
         type: '@?',
         name: '@?',
         autocomplete: '@?',
+        noAutocomplete: '@?',
         interface: '=?',
         noDebounce: '@?',
         match: '<?',
         syncCheck: '&?',
         asyncCheck: '&?',
-        checkMessage: '@?'
+        checkMessage: '@?',
+        sync: '&?',
+        disabled: '<?',
+        _focus: '<?focus',
+        suggestions: '<?'
+    },
+    transclude: {
+        left: '?uiInputLeft',
+        right: '?uiInputRight'
     }
 }
