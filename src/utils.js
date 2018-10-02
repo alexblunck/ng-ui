bab4af6e440f03c7ec4c3b8633fec20431f39841
Alexander Blunck <alex@blunck.co>
2018-10-02 13:56:58 +0200

    Added docblock to utils/debounce function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
+/**
+ * Debounce function.
+ * Source: https://gist.github.com/tcase360/3d0e370eca06189f025670d7dd40fe30#file-debounce-js
+ *
+ * @param  {Function} fn
+ * @param  {Integer}  time
+ *
+ * @return {Function}
+ */
 export const debounce = (fn, time) => {
     let timeout
 
